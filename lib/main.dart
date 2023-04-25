45fd1cdd4c29eeac9b639b65d6a027591cfe7fb8
Guilherme <guilhermemagno09@gmail.com>
2023-04-24 22:11:17 -0300

    building music player

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +9,162 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Music Player',
+      home: MusicPlayer(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MusicPlayer extends StatefulWidget {
+  const MusicPlayer({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MusicPlayer> createState() => _MusicPlayerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MusicPlayerState extends State<MusicPlayer> {
+  bool isPlaying = false;
+  double value = 0;
+  final player = AudioPlayer();
+  late Duration? duration = Duration(seconds: 0);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initPlayer() async {
+    await player.setSource(AssetSource('Dark_Necessities.mp3'));
+    duration = await player.getDuration();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPlayer();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/cover.jpeg'),
+                fit: BoxFit.cover,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 28, sigmaY: 28),
+              child: Container(
+                color: Colors.black54,
+              ),
             ),
-          ],
-        ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.asset(
+                  'assets/cover.jpeg',
+                  width: 250,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Dark Necessities',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  letterSpacing: 6,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '${(value / 60).floor()}:${(value % 60).floor()}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Slider.adaptive(
+                    onChanged: (v) {
+                      value = v;
+                    },
+                    min: 0.0,
+                    value: value,
+                    max: duration!.inSeconds.toDouble() + 1.0,
+                    onChangeEnd: (newValue) async {
+                      setState(() {
+                        value = newValue;
+                      });
+                      player.pause();
+                      await player.seek(Duration(seconds: newValue.toInt()));
+                      await player.resume();
+                    },
+                    activeColor: Colors.white,
+                  ),
+                  Text(
+                    "${duration!.inMinutes} : ${duration!.inSeconds % 60}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(60),
+                  border: Border.all(color: Colors.purple),
+                  color: Colors.black87,
+                ),
+                child: InkWell(
+                  onTap: () async {
+                    if (isPlaying) {
+                      await player.pause();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      duration = await player.getDuration();
+                      await player.resume();
+                      setState(() {
+                        isPlaying = true;
+                      });
+
+                      player.onPositionChanged.listen(
+                        (position) {
+                          setState(() {
+                            value = position.inSeconds.toDouble();
+                          });
+                        },
+                      );
+                    }
+                  },
+                  child: Icon(
+                    isPlaying ? Icons.pause : Icons.play_arrow,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
